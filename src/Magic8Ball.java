6ba97532af1e214ab4c71f93f4bef98a7b8ad51f
Henos Elias <henos.elias.2021@student.lhschools.org>
2018-10-02 07:41:59 -0400

    Created intelij project

---
+++ src/Magic8Ball.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Magic8Ball {
+
+    public static void main(String[]args) {
+     //declare
+     Scanner keyboard;
+     int testing;
+     String question;
+
+     //initialization phase
+        
+
+
+
+
+
+
+
+
+    }
+}
