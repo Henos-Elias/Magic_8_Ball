e2f2648378b0b76df49a2087d8db36db68b81907
Henos Elias <henos.elias.2021@student.lhschools.org>
2018-10-18 08:45:54 -0400

    Finished additionals

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -4,57 +4,70 @@ import java.util.Scanner;
 public class Magic8Ball {
 
     public static void main(String[]args) {
-     //declare
+            //declare
+     //int firstNumber = 10
+     //int secondNumber = 69;
+     //if (firstNumber>secondNumber);
+     //System.out.println(firstNumber + " is greater than " + secondNumber):
+
+
      Scanner keyboard;
      int testing;
      String question;
+     String again;
 
-     //initialization phase
-        keyboard= new Scanner(System.in);
-        System.out.println("what's your question?");
-        question=keyboard.nextLine();
-        System.out.println("put in a random number between 1-100");
-        testing=keyboard.nextInt();
-        if ((1 <=testing) &&(testing <=10)) {
-            System.out.println("your outlook looks good");
-        }
+     //initialization
+        do {
+            keyboard = new Scanner(System.in);
+            System.out.println("what's your question?");
+            question = keyboard.nextLine();
+            System.out.println("put in a random number between 1-100");
+            testing = keyboard.nextInt();
+            if ((1 <= testing) && (testing <= 10)) {
+                System.out.println("your outlook looks good");
+            }
 
-        if ((11<=testing)&&(testing<=20))  {
-            System.out.println("it is certain");
-        }
+            if ((11 <= testing) && (testing <= 20)) {
+                System.out.println("it is certain");
+            }
 
-        if ((21<=testing)&&(testing<=30))  {
-            System.out.println("it will pass");
-        }
+            if ((21 <= testing) && (testing <= 30)) {
+                System.out.println("it will pass");
+            }
 
-        if ((31<=testing)&&(testing<=40))  {
-            System.out.println("don't count on it");
-        }
+            if ((31 <= testing) && (testing <= 40)) {
+                System.out.println("don't count on it");
+            }
 
-        if ((41<=testing)&&(testing<=50))  {
-            System.out.println("sometime in the near future");
-        }
+            if ((41 <= testing) && (testing <= 50)) {
+                System.out.println("sometime in the near future");
+            }
 
-        if ((51<=testing)&&(testing<=60))  {
-            System.out.println("not so likely");
-        }
+            if ((51 <= testing) && (testing <= 60)) {
+                System.out.println("not so likely");
+            }
 
-        if ((61<=testing)&&(testing<=70))  {
-            System.out.println("probably not");
-        }
+            if ((61 <= testing) && (testing <= 70)) {
+                System.out.println("probably not");
+            }
 
-        if ((71<=testing)&&(testing<=80))  {
-            System.out.println("probably");
-        }
+            if ((71 <= testing) && (testing <= 80)) {
+                System.out.println("probably");
+            }
 
-        if ((81<=testing)&&(testing<=90))  {
-            System.out.println("absolutely");
-        }
+            if ((81 <= testing) && (testing <= 90)) {
+                System.out.println("absolutely");
+            }
 
-        if ((91<=testing)&&(testing<=100)) {
-            System.out.println("not a chance");
-        }
+            if ((91 <= testing) && (testing <= 100)) {
+                System.out.println("not a chance");
+            }
 
+            System.out.println("would you like to play again(yes or no)?");
+            again = keyboard.next();
+
+        }while (again.equals("yes"));
+            System.out.println("Goodbye");
 
 
 
